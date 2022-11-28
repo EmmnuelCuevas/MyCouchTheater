efd4715b11e28994b73c72abbdd5129dcbfa8e46
Emmanuel Cuevas <emmanuelcuevasm1@gmail.com>
2022-11-27 22:24:36 -0400

    basic ui, footer and nav created.

--- mycouchtheater/src/app/app.module.ts
+++ mycouchtheater/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { NavComponent } from './shared/nav/nav.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { NavComponent } from './shared/nav/nav.component';
     NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
